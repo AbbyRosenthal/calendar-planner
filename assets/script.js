2832105fdee155280dc56e7008e99cba0369b4b4
Abby Rosenthal <adlundeen@gmail.com>
2022-05-29 23:56:56 -0500

    save button class for styling

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,7 @@
 DateTime.fromObject({ outputCalendar: c }).toLocaleString(DateTime.DATE_FULL);
+
+//time dispaly at top ..WORK IN CONSOLE LOG, WHY DOES IT NOT DISPLAY ON MY PAGE?
+var displayTime = document.querySelector("#currentday")
+var currentTime = moment();
+
+displayTime.textContent = currentTime.format('MMMM Do YYYY, h:mm:ss a')
